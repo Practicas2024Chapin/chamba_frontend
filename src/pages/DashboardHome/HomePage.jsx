bdc4514b89343462f30d2263a0eea87011e6bbf0
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-09-12 17:21:26 -0600

    Cambios en el navbar e inputNav

---
+++ src/pages/DashboardHome/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+
+const HomePage = () => {
+    return (
+        <>
+            <Navbar />
+            <div className='text-black bg-white w-full h-[100vh]'>HomePage</div>
+        </>
+    )
+}
+
+export default HomePage
